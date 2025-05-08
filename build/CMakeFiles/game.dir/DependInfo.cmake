f794ceaa43294f71818c183a74171989d58d5a29
7ankalis <taha.marouani.1@gmail.com>
2025-05-09 00:26:15 +0100

    Fixed structure

---
+++ build/CMakeFiles/game.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/salamnki/7ankalis/village/src/Board.cpp" "CMakeFiles/game.dir/src/Board.cpp.o" "gcc" "CMakeFiles/game.dir/src/Board.cpp.o.d"
+  "/home/salamnki/7ankalis/village/src/Building.cpp" "CMakeFiles/game.dir/src/Building.cpp.o" "gcc" "CMakeFiles/game.dir/src/Building.cpp.o.d"
+  "/home/salamnki/7ankalis/village/src/ElixirCollector.cpp" "CMakeFiles/game.dir/src/ElixirCollector.cpp.o" "gcc" "CMakeFiles/game.dir/src/ElixirCollector.cpp.o.d"
+  "/home/salamnki/7ankalis/village/src/Enemy.cpp" "CMakeFiles/game.dir/src/Enemy.cpp.o" "gcc" "CMakeFiles/game.dir/src/Enemy.cpp.o.d"
+  "/home/salamnki/7ankalis/village/src/Entity.cpp" "CMakeFiles/game.dir/src/Entity.cpp.o" "gcc" "CMakeFiles/game.dir/src/Entity.cpp.o.d"
+  "/home/salamnki/7ankalis/village/src/GoldMine.cpp" "CMakeFiles/game.dir/src/GoldMine.cpp.o" "gcc" "CMakeFiles/game.dir/src/GoldMine.cpp.o.d"
+  "/home/salamnki/7ankalis/village/src/InputManager.cpp" "CMakeFiles/game.dir/src/InputManager.cpp.o" "gcc" "CMakeFiles/game.dir/src/InputManager.cpp.o.d"
+  "/home/salamnki/7ankalis/village/src/Npc.cpp" "CMakeFiles/game.dir/src/Npc.cpp.o" "gcc" "CMakeFiles/game.dir/src/Npc.cpp.o.d"
+  "/home/salamnki/7ankalis/village/src/Player.cpp" "CMakeFiles/game.dir/src/Player.cpp.o" "gcc" "CMakeFiles/game.dir/src/Player.cpp.o.d"
+  "/home/salamnki/7ankalis/village/src/Position.cpp" "CMakeFiles/game.dir/src/Position.cpp.o" "gcc" "CMakeFiles/game.dir/src/Position.cpp.o.d"
+  "/home/salamnki/7ankalis/village/src/ResourceGenerator.cpp" "CMakeFiles/game.dir/src/ResourceGenerator.cpp.o" "gcc" "CMakeFiles/game.dir/src/ResourceGenerator.cpp.o.d"
+  "/home/salamnki/7ankalis/village/src/Resources.cpp" "CMakeFiles/game.dir/src/Resources.cpp.o" "gcc" "CMakeFiles/game.dir/src/Resources.cpp.o.d"
+  "/home/salamnki/7ankalis/village/src/TownHall.cpp" "CMakeFiles/game.dir/src/TownHall.cpp.o" "gcc" "CMakeFiles/game.dir/src/TownHall.cpp.o.d"
+  "/home/salamnki/7ankalis/village/src/Wall.cpp" "CMakeFiles/game.dir/src/Wall.cpp.o" "gcc" "CMakeFiles/game.dir/src/Wall.cpp.o.d"
+  "/home/salamnki/7ankalis/village/src/main.cpp" "CMakeFiles/game.dir/src/main.cpp.o" "gcc" "CMakeFiles/game.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,39 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.dir/src/Board.cpp.o"
+  "CMakeFiles/game.dir/src/Board.cpp.o.d"
+  "CMakeFiles/game.dir/src/Building.cpp.o"
+  "CMakeFiles/game.dir/src/Building.cpp.o.d"
+  "CMakeFiles/game.dir/src/ElixirCollector.cpp.o"
+  "CMakeFiles/game.dir/src/ElixirCollector.cpp.o.d"
+  "CMakeFiles/game.dir/src/Enemy.cpp.o"
+  "CMakeFiles/game.dir/src/Enemy.cpp.o.d"
+  "CMakeFiles/game.dir/src/Entity.cpp.o"
+  "CMakeFiles/game.dir/src/Entity.cpp.o.d"
+  "CMakeFiles/game.dir/src/GoldMine.cpp.o"
+  "CMakeFiles/game.dir/src/GoldMine.cpp.o.d"
+  "CMakeFiles/game.dir/src/InputManager.cpp.o"
+  "CMakeFiles/game.dir/src/InputManager.cpp.o.d"
+  "CMakeFiles/game.dir/src/Npc.cpp.o"
+  "CMakeFiles/game.dir/src/Npc.cpp.o.d"
+  "CMakeFiles/game.dir/src/Player.cpp.o"
+  "CMakeFiles/game.dir/src/Player.cpp.o.d"
+  "CMakeFiles/game.dir/src/Position.cpp.o"
+  "CMakeFiles/game.dir/src/Position.cpp.o.d"
+  "CMakeFiles/game.dir/src/ResourceGenerator.cpp.o"
+  "CMakeFiles/game.dir/src/ResourceGenerator.cpp.o.d"
+  "CMakeFiles/game.dir/src/Resources.cpp.o"
+  "CMakeFiles/game.dir/src/Resources.cpp.o.d"
+  "CMakeFiles/game.dir/src/TownHall.cpp.o"
+  "CMakeFiles/game.dir/src/TownHall.cpp.o.d"
+  "CMakeFiles/game.dir/src/Wall.cpp.o"
+  "CMakeFiles/game.dir/src/Wall.cpp.o.d"
+  "CMakeFiles/game.dir/src/main.cpp.o"
+  "CMakeFiles/game.dir/src/main.cpp.o.d"
+  "game"
+  "game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
